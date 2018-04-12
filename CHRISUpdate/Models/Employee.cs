7776e552ba742a8c86ec6c1c5bb66ec2254345ca
tsaunders-gsa <terry.saunders@gsa.gov>
2018-04-12 15:42:30 -0400

    Added parameters to update user data.  Fixed spelling issue.

--- CHRISUpdate/Models/Employee.cs
+++ CHRISUpdate/Models/Employee.cs
@@ -7,7 +7,7 @@ namespace HRUpdate.Models
         public Person Person { get; set; }
         public Address Address { get; set; }
         public Birth Birth { get; set; }
-        public Investigation Investigaton { get; set; }
+        public Investigation Investigation { get; set; }
         public Emergency Emergency { get; set; }
         public Position Position { get; set; }
         public Phone Phone { get; set; }
