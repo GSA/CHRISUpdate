efae4c09474d94af7fccd446be77400f6b1d7fcc
tsaunders-gsa <terry.saunders@gsa.gov>
2018-04-23 10:04:46 -0400

    Added Fluent Validation.

---
+++ CHRISUpdate/Validation/ValidateHR.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using HRUpdate.Models;
+using System.Collections.Generic;
+
+namespace HRUpdate.Validation
+{
+    class ValidateHR
+    {
+        private readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ValidateHR()
+        {
+
+        }
+
+        private void ValidateEmployeeInformation(List<Employee> employeeInformation)
+        {
+            EmployeeValidator validator = new EmployeeValidator();
+        }
+    }
+
+    class EmployeeValidator :AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure;
+        }
+    }
+}
