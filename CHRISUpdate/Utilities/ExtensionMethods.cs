9788fd1eb7a599ed498bf5a403b4ce9e7da8304f
jgoodell-gsa <jason.goodell@gsa.gov>
2018-05-15 15:52:53 -0400

    update copy function and add extension methods for ssn and phone formatting

---
+++ CHRISUpdate/Utilities/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HRUpdate.Utilities
+{
+    public static class ExtensionMethods
+    {
+        public static string RemovePhoneFormatting(this string s)
+        {
+            return Regex.Replace(s, "[^0-9]", string.Empty);
+        }
+
+        public static string RemoveSocialFormatting(this string s)
+        {
+            return Regex.Replace(s, "[^0-9]", string.Empty);
+        }
+
+        //public static string RemoveZipFormatting
+    }
+}
