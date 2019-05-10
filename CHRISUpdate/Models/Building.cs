6d8a942393cce740fec4f7f4f49869e9f722a32c
jgoodell-gsa <jason.goodell@gas.gov>
2019-05-10 10:55:38 -0400

    remove white space

--- CHRISUpdate/Models/Building.cs
+++ CHRISUpdate/Models/Building.cs
@@ -6,7 +6,7 @@
         public string BuildingNumber { get; set; }
         public string BuildingAddress1 { get; set; }
         public string BuildingCity { get; set; }
-        public string BuildingState { get; set; }   
+        public string BuildingState { get; set; }
         public string BuildingZipCode { get; set; }
     }
 }           
